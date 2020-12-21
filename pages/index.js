431200574a2156f30ee531f8b9177362bb81c447
Yannick Gloster <yannickgloster@gmail.com>
2020-12-21 09:55:23 -0800

    Sample Github request for assignment

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,35 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 import { Octokit } from "@octokit/rest";
 
 export default function Home() {
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_API_KEY,
+  });
+
+  async () => {
+    const { data: pullRequest } = await octokit.pulls.get({
+      owner: "octokit",
+      repo: "rest.js",
+      pull_number: 123,
+    });
+    console.log(pullRequest);
+  };
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>GitHub Visualization</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <a href="https://github.com/">GitHub</a> Visualization
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -57,10 +70,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
+  );
 }
