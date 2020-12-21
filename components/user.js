4db35f8a7189c097e78651c560814cac41e368a1
Yannick Gloster <yannickgloster@gmail.com>
2020-12-21 14:08:35 -0800

    Created User Query that Displays Logo

--- components/user.js
+++ components/user.js
@@ -1,48 +1,56 @@
 import React from "react";
 import { Octokit } from "@octokit/rest";
 
-async function get_github(user) {
-  const octokit = new Octokit({
-    auth: process.env.GITHUB_API_KEY,
-  });
-
-  const data = await octokit.users.getByUsername({
-    username: user,
-  });
-  console.log(data);
-}
-
 class User extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: "" };
+    this.state = { value: "", data: null };
+    this.request = {};
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  async get_github(user) {
+    const octokit = new Octokit({
+      auth: process.env.GITHUB_API_KEY,
+    });
+
+    const data = await octokit.users.getByUsername({
+      username: user,
+    });
+    return data;
   }
 
   handleChange(event) {
     this.setState({ value: event.target.value });
   }
 
-  handleSubmit(event) {
+  async handleSubmit(event) {
     event.preventDefault();
-    get_github(this.state.value);
+    this.request = await this.get_github(this.state.value);
+    this.setState({ data: this.request["data"]["avatar_url"] });
+    console.log(this.state);
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Github Account:{" "}
-          <input
-            type="text"
-            value={this.state.value}
-            onChange={this.handleChange}
-          />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Github Account:{" "}
+            <input
+              type="text"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        {this.state.data != null && (
+          <img src={this.state.data} alt="Profile Image" />
+        )}
+      </div>
     );
   }
 }
