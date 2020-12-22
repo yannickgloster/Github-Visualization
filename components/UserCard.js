0fe5966004c6b0a3b44ebf5e6320709cf669cecd
Yannick Gloster <yannickgloster@gmail.com>
2020-12-21 21:16:37 -0800

    Update UserCard
    Preliminary work on UserCard

--- components/UserCard.js
+++ components/UserCard.js
@@ -1,6 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styles from "./UserCard.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBuilding,
+  faCompass,
+  faUser,
+} from "@fortawesome/free-regular-svg-icons";
 
 class UserCard extends React.Component {
   constructor(props) {
@@ -10,37 +16,55 @@ class UserCard extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className={styles.outerContainer}>
         <img
-          className={styles.profile}
+          className={styles.profile_img}
           src={this.props.image_url}
           alt="Profile Image"
         />
-        <h1>{this.props.name}</h1>
-        <h2>
-          <a href={this.props.profile_url} target="_blank">
-            {this.props.username}
-          </a>
-        </h2>
-        <p>{this.props.bio}</p>
-        <p>Works at {this.props.company}</p>
-        <p>
-          Location:{" "}
-          <a
-            href={
-              "https://www.google.com/maps/search/?api=1&query=" +
-              this.props.location
-            }
-            target="_blank"
-          >
-            {this.props.location}
-          </a>
-        </p>
-        <p>Repos: {this.props.public_repos}</p>
-        <p>Gists: {this.props.public_gists}</p>
-        <p>Followers: {this.props.followers}</p>
-        <p>Following: {this.props.following}</p>
-        <p>Member since {this.props.created_at.slice(0, 4)}!</p>
+        <div className={styles.innerContainer}>
+          <h1>{this.props.name}</h1>
+          <h2>
+            <a href={this.props.profile_url} target="_blank">
+              {this.props.username}
+            </a>
+          </h2>
+          <p>{this.props.bio}</p>
+          <div className={styles.item}>
+            <FontAwesomeIcon icon={faBuilding} className={styles.icon} />
+            <p>{this.props.company}</p>
+          </div>
+          <div className={styles.item}>
+            <FontAwesomeIcon icon={faCompass} className={styles.icon} />
+            <p>
+              <a
+                href={
+                  "https://www.google.com/maps/search/?api=1&query=" +
+                  this.props.location
+                }
+                target="_blank"
+              >
+                {this.props.location}
+              </a>
+            </p>
+          </div>
+          <div className={styles.item}>
+            <img
+              src="/github-icon.svg"
+              alt="GitHub Octocat Logo"
+              className={styles.icon}
+            />
+            <p>Repos: {this.props.public_repos}</p>
+            <p>Gists: {this.props.public_gists}</p>
+          </div>
+          <div className={styles.item}>
+            <FontAwesomeIcon icon={faUser} className={styles.icon} />
+            <p>Followers: {this.props.followers}</p>
+
+            <p>Following: {this.props.following}</p>
+          </div>
+          <p>Member since {this.props.created_at.slice(0, 4)}!</p>
+        </div>
       </div>
     );
   }
